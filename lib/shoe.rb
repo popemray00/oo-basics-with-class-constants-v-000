65b7a0434fd3486d395db9d9bf01bb0c446937aa
Michael Pope <popemray1986@gmail.com>
2017-11-12 18:41:09 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -16,6 +16,5 @@ class Shoe
   def brand=(brand)
     @brand = brand
     BRANDS << brand
-    BRANDS.size = 3
   end
 end
